e5e7e6fb472cf24c8c75416a539a20c68f8afd49
Robert Mannuzza <Robert.mannuzza@gmail.com>
2015-03-17 16:53:45 -0400

    small changes

--- src/com/cbthinkx/puzzler/menu/UploadScreen.java
+++ src/com/cbthinkx/puzzler/menu/UploadScreen.java
@@ -9,17 +9,23 @@ public class UploadScreen extends Background{
 		super();
 		JButton upld = new JButton("Upload");
 		upld.setSize(100, 40);
-        upld.setLocation(0, (int)(pframe.getHeight() * 0.45));
+        upld.setLocation(
+                pframe.getWidth() / 2 - upld.getWidth() / 2,
+                (int)(pframe.getHeight() * 0.25)
+        );
 		add(upld);
 		JTextField size = new JTextField();
 		size.setSize(100, 40);
-        size.setLocation(0, (int)(pframe.getHeight() * 0.55));
+        size.setLocation(
+                pframe.getWidth() / 2 - upld.getWidth() / 2,
+                (int)(pframe.getHeight() * 0.45)
+        );
 		add(size);
 		JButton next = new JButton("next");
 		next.setSize(100, 40);
         next.setLocation(
                 pframe.getWidth() / 2 - next.getWidth() / 2 ,
-                (int)(pframe.getHeight() * 0.85)
+                (int)(pframe.getHeight() * 0.65)
         );
 		add(next);
 	}
