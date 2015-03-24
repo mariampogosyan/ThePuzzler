b5c7224a65c628be9e1a56c06d3d1b96c849f34b
Mariam Pogosyan <vmariam.pogosyan@gmail.com>
2015-03-23 20:19:07 -0400

    cropping

--- src/com/cbthinkx/puzzler/CoreService/ImageUtility.java
+++ src/com/cbthinkx/puzzler/CoreService/ImageUtility.java
@@ -2,10 +2,15 @@ package com.cbthinkx.puzzler.CoreService;
 
 
 import javax.imageio.ImageIO;
+
+import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 public class ImageUtility {
     public BufferedImage newImage (double size, PuzzleShape pShape, BufferedImage img) {
@@ -33,9 +38,50 @@ public class ImageUtility {
         dim[2] = k;
         return dim;
     }
-
     private BufferedImage cropImage(BufferedImage img, PuzzleShape pShape) {
+    	switch (pShape) {
+    		case SQUARE: {
+    			return img;
+    		}
+    		case ELLIPSE: {
+    			return ellipseCrop(img);
+    		}
+    		case HEART: {
+    			return heartCrop(img);
+    		}
+    		case NGON: {
+    			return ngonCrop(img);
+    		}
+    		default: {
+    			break;
+    		}
+    	}
         return img;
+    }
+    
+    private BufferedImage ellipseCrop (BufferedImage img) {
+    	int h = img.getHeight();
+    	int w = img.getWidth();
+    	Ellipse2D.Double ellipse = new Ellipse2D.Double(0, 0, w, h);
+        Graphics2D g2d = img.createGraphics();
+        g2d.setClip(ellipse);
+        g2d.drawImage(img, null, 0, 0);
+        Rectangle2D rect = new Rectangle2D.Double();
+        rect.setRect(0, 0, w, h);
+        g2d.clip(rect);
+        try {
+			ImageIO.write(img, "jpg", new File ("cropped.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return img;    	
+    }
+    private BufferedImage heartCrop (BufferedImage img) {
+    	return img;
+    }
+    private BufferedImage ngonCrop (BufferedImage img) {
+    	return img;
     }
 
 }
