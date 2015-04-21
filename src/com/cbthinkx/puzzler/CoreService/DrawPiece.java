d84db5cb620559114c8e22394499d8c2631dd879
Mariam Pogosyan <vmariam.pogosyan@gmail.com>
2015-04-20 20:28:32 -0400

    stuff

--- src/com/cbthinkx/puzzler/CoreService/DrawPiece.java
+++ src/com/cbthinkx/puzzler/CoreService/DrawPiece.java
@@ -1,6 +1,7 @@
 package com.cbthinkx.puzzler.CoreService;
 import java.awt.*;
 import java.awt.geom.*;
+import java.awt.image.BufferedImage;
 import java.util.Random;
 
 import javax.swing.*;
@@ -105,4 +106,137 @@ public class DrawPiece extends JFrame {
 				return p2dd;
 			}
 		}
+
+//		private BufferedImage combineVertical(BufferedImage cur, BufferedImage bot) {
+//			int height = cur.getHeight() + bot.getHeight();
+//			int width = Math.max(cur.getWidth(), bot.getWidth());
+//			BufferedImage fin = new BufferedImage(width, height, cur.getType());
+//			Graphics2D g2 = fin.createGraphics();
+//			g2.drawImage(cur, null, 0, 0);
+//			g2.drawImage(bot, null, 0, cur.getHeight());
+//			g2.dispose();
+//			return fin;
+//		}
+//		private BufferedImage combineHorizontal(BufferedImage cur, BufferedImage right) {
+//			int height = Math.max(cur.getHeight(), right.getHeight());
+//			int width = cur.getWidth() + right.getWidth();
+//			BufferedImage fin = new BufferedImage(width, height, cur.getType());
+//			Graphics2D g2 = fin.createGraphics();
+//			g2.drawImage(cur, null, 0, 0);
+//			g2.drawImage(right, null, cur.getWidth(), 0);
+//			g2.dispose();
+//			return fin;
+//		}
+//		private BufferedImage jigSawVertical(BufferedImage img, boolean isCur) {
+//			BufferedImage fin = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_ARGB);
+//			if (isCur) {
+//				AffineTransform tran = new AffineTransform();
+//				tran.translate(
+//						img.getWidth() /256,
+//						img.getHeight() / 2
+//				);
+//				tran.rotate(Math.PI / 2);
+//				Path2D p2d = new Path2D.Double();
+//				p2d.moveTo(-img.getHeight() / 2, 0);
+//				p2d.lineTo(0, 0);
+//				p2d = addCurvePath(img.getHeight() / 2, img.getWidth(), p2d);
+//				p2d.lineTo(-img.getWidth(), -img.getHeight() / 2);
+//				p2d.closePath();
+//				Shape sp = p2d.createTransformedShape(tran);
+//				Graphics2D g2 = fin.createGraphics();
+//				g2.setColor(new Color(0, true));
+//				g2.fillRect(0, 0, img.getWidth(), img.getHeight());
+//				g2.setStroke(new BasicStroke(6.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+//				g2.setClip(sp);
+//				g2.drawImage(img, null, 0, 0);
+//				g2.dispose();
+//				BufferedImage crop = new BufferedImage(fin.getWidth(), (int)(fin.getHeight() * (.68)), BufferedImage.TYPE_INT_ARGB);
+//				Graphics2D g2dd = crop.createGraphics();
+//				g2dd.drawImage(fin, null, 0, 0);
+//				g2dd.dispose();
+//				return crop;
+//			} else {
+//				AffineTransform tran = new AffineTransform();
+//				tran.translate(
+//						img.getWidth() /256,
+//						img.getHeight() / 2
+//				);
+//				tran.rotate(Math.PI / 2);
+//				Path2D p2d = new Path2D.Double();
+//				p2d.moveTo(img.getHeight() / 2, 0);
+//				p2d.lineTo(0, 0);
+//				p2d = addCurvePath(img.getHeight() / 2, img.getWidth(), p2d);
+//				p2d.lineTo(img.getWidth(), -img.getHeight() / 2);
+//				p2d.closePath();
+//				Shape sp = p2d.createTransformedShape(tran);
+//				Graphics2D g2 = fin.createGraphics();
+//				g2.setColor(new Color(0, true));
+//				g2.fillRect(0, 0, img.getWidth(), img.getHeight());
+//				g2.setStroke(new BasicStroke(6.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+//				g2.setClip(sp);
+//				g2.drawImage(img, null, 0, 0);
+//				g2.dispose();
+//				BufferedImage crop = new BufferedImage(fin.getWidth(), fin.getHeight()/2, BufferedImage.TYPE_INT_ARGB);
+//				Graphics2D g2dd = crop.createGraphics();
+//				g2dd.drawImage(fin, null, 0, -fin.getHeight() / 2);
+//				g2dd.dispose();
+//				return crop;
+//			}
+//		}
+//		private BufferedImage jigSawHorizontal(BufferedImage img, boolean isCur) {
+//			BufferedImage fin = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_ARGB);
+//			if (isCur) {
+//				AffineTransform tran = new AffineTransform();
+//				tran.translate(
+//						img.getWidth() /2,
+//						img.getHeight() / 256
+//				);
+//				tran.rotate(Math.PI);
+//				Path2D p2d = new Path2D.Double();
+//				p2d.moveTo(img.getHeight(), 0);
+//				p2d.lineTo(0, 0);
+//				p2d = addCurvePath(img.getHeight(), img.getWidth() / 2, p2d);
+//				p2d.lineTo(img.getWidth() / 2, -img.getHeight());
+//				p2d.closePath();
+//				Shape sp = p2d.createTransformedShape(tran);
+//				Graphics2D g2 = fin.createGraphics();
+//				g2.setColor(new Color(0, true));
+//				g2.fillRect(0, 0, img.getWidth(), img.getHeight());
+//				g2.setStroke(new BasicStroke(6.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+//				g2.setClip(sp);
+//				g2.drawImage(img, null, 0, 0);
+//				g2.dispose();
+//				BufferedImage crop = new BufferedImage(fin.getWidth() / 2,fin.getHeight(), BufferedImage.TYPE_INT_ARGB);
+//				Graphics2D g2dd = crop.createGraphics();
+//				g2dd.drawImage(fin, null, 0, 0);
+//				g2dd.dispose();
+//				return crop;
+//			} else {
+//				AffineTransform tran = new AffineTransform();
+//				tran.translate(
+//						img.getWidth() /2,
+//						img.getHeight() / 256
+//				);
+//				tran.rotate(Math.PI);
+//				Path2D p2d = new Path2D.Double();
+//				p2d.moveTo(-img.getHeight(), 0);
+//				p2d.lineTo(0, 0);
+//				p2d = addCurvePath(img.getHeight(), img.getWidth() / 2, p2d);
+//				p2d.lineTo(-img.getWidth() / 2, -img.getHeight());
+//				p2d.closePath();
+//				Shape sp = p2d.createTransformedShape(tran);
+//				Graphics2D g2 = fin.createGraphics();
+//				g2.setColor(new Color(0, true));
+//				g2.fillRect(0, 0, img.getWidth(), img.getHeight());
+//				g2.setStroke(new BasicStroke(6.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+//				g2.setClip(sp);
+//				g2.drawImage(img, null, 0, 0);
+//				g2.dispose();
+//				BufferedImage crop = new BufferedImage((int)(fin.getWidth() * (0.68)),fin.getHeight(), BufferedImage.TYPE_INT_ARGB);
+//				Graphics2D g2dd = crop.createGraphics();
+//				g2dd.drawImage(fin, null, (int)(-fin.getWidth() / 2 ), 0);
+//				g2dd.dispose();
+//				return crop;
+//			}
+//		}
 }
